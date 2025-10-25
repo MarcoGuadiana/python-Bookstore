9780656773e2e46e5ba0a546c38cd58ea17c9a10
Marco Guadiana <marcoguadiana01@gmail.com>
2025-10-25 08:39:29 -0700

    Added Book attributes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3Book
+#* Attributes:
+ # * title
+ # * page_Count
 
 class Book:
-    pass
+    def __init__(title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+
+
     
-        
+                
